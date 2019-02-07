9b585a1eaaf776650c2a57add2d0d7a04a6d55e7
AaronCoplan <afcoplan@gmail.com>
2019-02-07 12:57:40 -0500

    Setup small example with handling of syntax errors.

--- src/main/java/org/aaroncoplan/waterfall/App.java
+++ src/main/java/org/aaroncoplan/waterfall/App.java
@@ -2,15 +2,17 @@ package org.aaroncoplan.waterfall;
 
 import com.aaroncoplan.waterfall.WaterfallLexer;
 import com.aaroncoplan.waterfall.WaterfallParser;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
     public String getGreeting() {
         return "Hello world.";
     }
@@ -34,9 +36,40 @@ public class App {
         final CharStream charStream = CharStreams.fromString(code.toString());
         final WaterfallLexer waterfallLexer = new WaterfallLexer(charStream);
         final CommonTokenStream tokenStream = new CommonTokenStream(waterfallLexer);
+
         final WaterfallParser waterfallParser = new WaterfallParser(tokenStream);
+        waterfallParser.removeErrorListeners();
+        ThrowingErrorListener errorListener = new ThrowingErrorListener(waterfallCodeFilePath);
+        waterfallParser.addErrorListener(errorListener);
 
         final WaterfallParser.ProgramContext programAST = waterfallParser.program();
-        System.out.println(programAST.toStringTree());
+
+        System.out.println("ERROR MESSAGES");
+        for(String msg : errorListener.getSyntaxErrors()) {
+            System.out.println(msg);
+        }
+
+        System.out.println(programAST.toStringTree(waterfallParser));
+    }
+}
+
+class ThrowingErrorListener extends BaseErrorListener {
+
+    private final String name;
+    private final List<String> syntaxErrors;
+
+    public ThrowingErrorListener(String name) {
+        this.name = name;
+        this.syntaxErrors = new ArrayList<String>();
+    }
+
+    public List<String> getSyntaxErrors() {
+        return syntaxErrors;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) throws ParseCancellationException {
+        final String errorMessage = name + " line " + line + ":" + charPositionInLine + " " + msg;
+        syntaxErrors.add(errorMessage);
     }
 }
