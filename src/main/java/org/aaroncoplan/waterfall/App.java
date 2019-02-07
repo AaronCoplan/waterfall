0f4d5e6a4057a45cc075737c3f97b3a2d7690adb
AaronCoplan <afcoplan@gmail.com>
2019-02-07 16:37:33 -0500

    Formatted code with prettier.

--- src/main/java/org/aaroncoplan/waterfall/App.java
+++ src/main/java/org/aaroncoplan/waterfall/App.java
@@ -2,67 +2,85 @@ package org.aaroncoplan.waterfall;
 
 import com.aaroncoplan.waterfall.WaterfallLexer;
 import com.aaroncoplan.waterfall.WaterfallParser;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.misc.ParseCancellationException;
 
-import javax.print.DocFlavor;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.print.DocFlavor;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
 public class App {
 
-    public String getGreeting() {
-        return "Hello world.";
+  public String getGreeting() {
+    return "Hello world.";
+  }
+
+  public static void main(String[] args) {
+    // open file and read it in
+    final String waterfallCodeFilePath = args[0];
+    final StringBuilder code = new StringBuilder();
+    try {
+      Scanner sc = new Scanner(new File(waterfallCodeFilePath));
+      while (sc.hasNextLine()) {
+        code.append(sc.nextLine() + "\n");
+      }
+      sc.close();
+    } catch(FileNotFoundException e) {
+      System.err.format(
+        "File %s could not be found",
+        waterfallCodeFilePath
+      ).println();
+      System.exit(-1);
     }
+    // parse using ANTLR
+    final CharStream charStream = CharStreams.fromString(code.toString());
+    final WaterfallLexer waterfallLexer = new WaterfallLexer(charStream);
+    final CommonTokenStream tokenStream = new CommonTokenStream(waterfallLexer);
 
-    public static void main(String[] args) {
-        // open file and read it in
-        final String waterfallCodeFilePath = args[0];
-        final StringBuilder code = new StringBuilder();
-        try {
-            Scanner sc = new Scanner(new File(waterfallCodeFilePath));
-            while(sc.hasNextLine()) {
-                code.append(sc.nextLine() + "\n");
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.err.format("File %s could not be found", waterfallCodeFilePath).println();
-            System.exit(-1);
-        }
+    final WaterfallParser waterfallParser = new WaterfallParser(tokenStream);
+    waterfallParser.removeErrorListeners();
+    SyntaxErrorListener errorListener = new SyntaxErrorListener(
+      waterfallCodeFilePath
+    );
+    waterfallParser.addErrorListener(errorListener);
 
-        // parse using ANTLR
-        final CharStream charStream = CharStreams.fromString(code.toString());
-        final WaterfallLexer waterfallLexer = new WaterfallLexer(charStream);
-        final CommonTokenStream tokenStream = new CommonTokenStream(waterfallLexer);
+    final WaterfallParser.ProgramContext programAST = waterfallParser.program();
+    System.out.println(programAST.toStringTree(waterfallParser));
 
-        final WaterfallParser waterfallParser = new WaterfallParser(tokenStream);
-        waterfallParser.removeErrorListeners();
-        SyntaxErrorListener errorListener = new SyntaxErrorListener(waterfallCodeFilePath);
-        waterfallParser.addErrorListener(errorListener);
-
-        final WaterfallParser.ProgramContext programAST = waterfallParser.program();
-        System.out.println(programAST.toStringTree(waterfallParser));
-
-        final List<String> syntaxErrors = errorListener.getSyntaxErrors();
-        if(syntaxErrors.size() > 0) {
-            System.out.println("ERROR MESSAGES");
-            for (String msg : syntaxErrors) {
-                System.out.println(msg);
-            }
-            System.exit(-1);
-        }
-
-        programAST.code().codeline().forEach(clCtx -> {
-            WaterfallParser.VariableDeclarationContext variableDeclarationContext = clCtx.variableDeclaration();
-            System.out.println(variableDeclarationContext.variableType().ID().getText());
-            System.out.println(variableDeclarationContext.variableName().ID().getText());
-            System.out.println(variableDeclarationContext.variableValue().INT_LITERAL());
-            System.out.println(variableDeclarationContext.variableValue().DEC_LITERAL());
-            System.out.println();
-        });
+    final List<String> syntaxErrors = errorListener.getSyntaxErrors();
+    if (syntaxErrors.size() > 0) {
+      System.out.println("ERROR MESSAGES");
+      for (String msg : syntaxErrors) {
+        System.out.println(msg);
+      }
+      System.exit(-1);
     }
+    programAST.code().codeline().forEach(
+      clCtx -> {
+        WaterfallParser.VariableDeclarationContext variableDeclarationContext = clCtx.variableDeclaration(
+
+        );
+        System.out.println(
+          variableDeclarationContext.variableType().ID().getText()
+        );
+        System.out.println(
+          variableDeclarationContext.variableName().ID().getText()
+        );
+        System.out.println(
+          variableDeclarationContext.variableValue().INT_LITERAL()
+        );
+        System.out.println(
+          variableDeclarationContext.variableValue().DEC_LITERAL()
+        );
+        System.out.println();
+      }
+    );
+  }
+
 }
 
--- src/main/java/org/aaroncoplan/waterfall/SyntaxErrorListener.java
+++ src/main/java/org/aaroncoplan/waterfall/SyntaxErrorListener.java
@@ -1,30 +1,41 @@
 package org.aaroncoplan.waterfall;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
-import java.util.ArrayList;
-import java.util.List;
+class SyntaxErrorListener
+  extends BaseErrorListener {
+  private final String name;
+  private final List<String> syntaxErrors;
 
-class SyntaxErrorListener extends BaseErrorListener {
+  public SyntaxErrorListener(String name) {
+    this.name = name;
+    this.syntaxErrors = new ArrayList<String>();
+  }
 
-    private final String name;
-    private final List<String> syntaxErrors;
+  public List<String> getSyntaxErrors() {
+    return syntaxErrors;
+  }
 
-    public SyntaxErrorListener(String name) {
-        this.name = name;
-        this.syntaxErrors = new ArrayList<String>();
-    }
+  @Override
+  public void syntaxError(
+    Recognizer<?, ?> recognizer,
+    Object offendingSymbol,
+    int line,
+    int charPositionInLine,
+    String msg,
+    RecognitionException e
+  )
+    throws
+      ParseCancellationException {
+    final String errorMessage = name + " line " + line + ":" + charPositionInLine + " " + msg;
+    syntaxErrors.add(errorMessage);
+  }
 
-    public List<String> getSyntaxErrors() {
-        return syntaxErrors;
-    }
-
-    @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) throws ParseCancellationException {
-        final String errorMessage = name + " line " + line + ":" + charPositionInLine + " " + msg;
-        syntaxErrors.add(errorMessage);
-    }
 }
+
