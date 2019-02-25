6378894ef7e66cb67f370436b85ceb8e56617127
AaronCoplan <afcoplan@gmail.com>
2019-02-25 02:43:59 -0500

    Implementing ParseResult data class.

--- src/main/java/org/aaroncoplan/waterfall/ParseResult.java
+++ src/main/java/org/aaroncoplan/waterfall/ParseResult.java
@@ -1,4 +1,23 @@
 package org.aaroncoplan.waterfall;
 
-public class ParseResult {}
+import com.aaroncoplan.waterfall.WaterfallParser;
+
+import java.util.List;
+
+public class ParseResult {
+    private final String filePath;
+    private final List<String> syntaxErrors;
+    private final WaterfallParser.ProgramContext programAST;
+
+    public ParseResult(
+        String filePath,
+        List<String> syntaxErrors,
+        WaterfallParser.ProgramContext programAST
+    ) {
+        this.filePath = filePath;
+        this.syntaxErrors = syntaxErrors;
+        this.programAST = programAST;
+    }
+
+}
 
