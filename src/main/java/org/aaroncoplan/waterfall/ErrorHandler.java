422e0a8a88da6658649d5059449f4683a675673e
AaronCoplan <afcoplan@gmail.com>
2019-02-25 11:41:06 -0500

    Making all error handling use global ErrorHandler exit point.

--- src/main/java/org/aaroncoplan/waterfall/ErrorHandler.java
+++ src/main/java/org/aaroncoplan/waterfall/ErrorHandler.java
@@ -2,6 +2,10 @@ package org.aaroncoplan.waterfall;
 
 public class ErrorHandler {
 
+    public static void exit() {
+        System.exit(-1);
+    }
+
     public static void exit(String errorMessageFormat, Object... args) {
         final String errorMessage = String.format(errorMessageFormat, args);
         System.out.format("[ERROR] %s", errorMessage).println();
