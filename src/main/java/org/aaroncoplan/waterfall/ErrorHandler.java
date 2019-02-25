5a68555eec440b16cbc7aef70962564d79991d75
AaronCoplan <afcoplan@gmail.com>
2019-02-25 11:35:31 -0500

    Added global ErrorHandler exit point.

---
+++ src/main/java/org/aaroncoplan/waterfall/ErrorHandler.java
@@ -0,0 +1,12 @@
+package org.aaroncoplan.waterfall;
+
+public class ErrorHandler {
+
+    public static void exit(String errorMessageFormat, Object... args) {
+        final String errorMessage = String.format(errorMessageFormat, args);
+        System.out.format("[ERROR] %s", errorMessage).println();
+        System.exit(-1);
+    }
+
+}
+
