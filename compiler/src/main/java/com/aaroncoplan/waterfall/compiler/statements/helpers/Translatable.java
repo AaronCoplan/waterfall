5b14213af482aeb0183dc9ddf8301205a7b5a4cf
AaronCoplan <afcoplan@gmail.com>
2019-12-01 21:50:06 -0800

    Removed all files to start from scratch.

--- compiler/src/main/java/com/aaroncoplan/waterfall/compiler/statements/helpers/Translatable.java
+++
@@ -1,8 +0,0 @@
-package com.aaroncoplan.waterfall.compiler.statements.helpers;
-
-import com.aaroncoplan.waterfall.compiler.symboltables.SymbolTable;
-
-public interface Translatable {
-    VerificationResult verify(SymbolTable symbolTable);
-    String translate();
-}
--- compiler/src/main/java/com/aaroncoplan/waterfall/compiler/symboltables/DuplicateDeclarationException.java
+++
@@ -1,6 +0,0 @@
-package com.aaroncoplan.waterfall.compiler.symboltables;
-
-public class DuplicateDeclarationException extends Exception {
-
-    private static final long serialVersionUID = 8784552564379536232L;
-}
